783cd3c3497f1d00c92118a29f1018f185d625fd
Alireza Meftahi <alirezameftahi_71@yahoo.com>
2018-11-20 20:30:10 +0330

    fixed height of add_update page

--- public/add_update.php
+++ public/add_update.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-  <div class="row content full-height justify-content-center">
+  <div id="full-height" class="row content justify-content-center">
     <div class="col-md-9">
       <h3 class="text-center mt-4 mb-4">
         <?php echo $page_title ?>
