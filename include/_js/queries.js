b6e138093198dc2aeb7342845024c7bcb5d337cf
Alireza Meftahi <alirezameftahi_71@yahoo.com>
2019-03-01 15:32:54 +0330

    each field has its own selectable ops based on it's type

--- include/_js/queries.js
+++ include/_js/queries.js
@@ -1,129 +1,180 @@
 var allFields;
-const allOperators = [{
-    title: 'Equal to',
-    value: '='
-}, {
-    title: 'Less than',
-    value: '<'
-}, {
-    title: 'More than',
-    value: '>'
-}, {
-    title: 'Less or equal',
-    value: '<='
-}, {
-    title: 'More or equal',
-    value: '>='
-}, {
-    title: 'Not Equal',
-    value: '<>'
-}, {
-    title: 'Contains',
-    value: 'like'
-}, {
-    title: 'Not contains',
-    value: 'not like'
-}];
+const allOperators = [
+  {
+    title: "Equal to",
+    value: "=",
+    types: ["int", "float", "varchar", "text", "nvarchar", "date"]
+  },
+  {
+    title: "Less than",
+    value: "<",
+    types: ["int", "float", "date"]
+  },
+  {
+    title: "More than",
+    value: ">",
+    types: ["int", "float", "date"]
+  },
+  {
+    title: "Less or equal",
+    value: "<=",
+    types: ["int", "float", "date"]
+  },
+  {
+    title: "More or equal",
+    value: ">=",
+    types: ["int", "float", "date"]
+  },
+  {
+    title: "Not Equal",
+    value: "<>",
+    types: ["int", "float", "varchar", "text", "nvarchar", "date"]
+  },
+  {
+    title: "Contains",
+    value: "like",
+    types: ["varchar", "text", "nvarchar"]
+  },
+  {
+    title: "Not contains",
+    value: "not like",
+    types: ["varchar", "text", "nvarchar"]
+  },
+  {
+    title: "Between",
+    value: "Between",
+    types: ["int", "float", "date"]
+  }
+];
 
-$(function () {
-    $('#add-row-btn').click(addRow);
-    _apiRequest(
-        "../include/_php/services/query_builder.php",
-        "POST",
-        null,
-        "json",
-        function (data) {
-            allFields = data;
-            addRow();
-        }
-    );
-    $('#btn-submit').click(sendQuery);
-    $('#btn-reset').click(resetForm);
+$(() => {
+  $("#add-row-btn").click(addRow);
+  _apiRequest(
+    "../include/_php/services/query_builder.php",
+    "POST",
+    null,
+    "json",
+    data => {
+      data.unshift({ field: "", type: "" });
+      allFields = data;
+      //addRow();
+    }
+  );
+  $("#btn-submit").click(sendQuery);
+  $("#btn-reset").click(resetForm);
+  $("table#query-builder").on("change", "#all-fields", e => {
+    let type = $(e.target.selectedOptions).attr("datatype");
+    let row = $(e.target).parents("tr");
+    let ops = allOperators.filter(e => {
+      return e.types.indexOf(type.split("(")[0]) > -1;
+    });
+    fillOperatorsDropdown(ops, row);
+  });
+  $("table#query-builder").on("change", "#operators", e => {
+    // on operator change
+  });
 });
 
 function addRow() {
-    let element = $('<tr><td>' +
-        '<input class="form-control btn btn-outline-dark" id="remove-row-btn" onclick="removeRow(this, event)" type="button" value="&#10005;">' +
-        '</td><td>' +
-        '<select id="all-fields" name="field" class="form-control">' +
-        '</select>' +
-        '</td><td>' +
-        '<select id="all-operators" name="mop" class="form-control">' +
-        '</select>' +
-        '</td><td>' +
-        '<input name="value" class="form-control" type="text">' +
-        '</td><td>' +
-        '<select name="cop" class="form-control">' +
-        '<option value="AND">AND</option>' +
-        '<option value="OR">OR</option>' +
-        '</select>' +
-        '</td></tr>');
-
-    for (let i = 0; i < allFields.length; i++) {
-        let fieldOption = $('<option></option>', {
-            value: allFields[i].field,
-            dataType: allFields[i].type,
-        });
-        fieldOption.text(allFields[i].field.toUpperCase());
-        $(element).find('#all-fields').append($(fieldOption));
-
-    }
-    for (let i = 0; i < allOperators.length; i++) {
-        let operatorOption = $('<option></option>', {
-            value: allOperators[i].value
-        });
-        operatorOption.text(allOperators[i].title);
-        $(element).find('#all-operators').append($(operatorOption));
-    }
-
-    $('table#query-builder tbody').append($(element));
+  const uid = uuidv4();
+  let row = $(`
+        <tr id=${uid} class="row-query">
+            <td>
+            <input
+                id="remove-row-btn"
+                class="form-control btn btn-outline-dark"
+                onclick="removeRow(this, event)"
+                type="button"
+                value="&#10005;"
+            />
+            </td>
+            <td><select id="all-fields" name="field" class="form-control"></select></td>
+            <td><select id="operators" name="mop" class="form-control"></select></td>
+            <td><input id="input-value" name="value" class="form-control" type="text" /></td>
+            <td>
+            <select name="cop" class="form-control">
+                <option value="AND">AND</option>
+                <option value="OR">OR</option>
+            </select>
+            </td>
+        </tr>
+    `);
+    fillFieldsDropdown(allFields, $(row));
+  $("table#query-builder tbody").append(row);
 }
 
-function removeRow(e, event) {
-    $(event.target).parents('tr').remove();
+function removeRow(e) {
+  $(e)
+    .parents("tr")
+    .remove();
 }
 
 function sendQuery() {
-    var data = $('form').serializeArray();
-    var processedData = prepareRequestData(data);
-    var processedDataStr = JSON.stringify(processedData);
-    _apiRequest(
-        "../include/_php/services/query_builder.php",
-        "POST",
-        processedDataStr,
-        "json",
-        function (result) {
-            $('#result-table').bootstrapTable('destroy');
-            $('#result-table').bootstrapTable({
-                data: result
-            }).show();
-        }
-    );
+  var data = $("form").serializeArray();
+  var processedData = prepareRequestData(data);
+  var processedDataStr = JSON.stringify(processedData);
+  _apiRequest(
+    "../include/_php/services/query_builder.php",
+    "POST",
+    processedDataStr,
+    "json",
+    result => {
+      $("#result-table").bootstrapTable("destroy");
+      $("#result-table")
+        .bootstrapTable({
+          data: result
+        })
+        .show();
+    }
+  );
 }
 
 function prepareRequestData(data) {
-    var res = {};
-    var temp = {};
-    var array = [];
-    for (var item of data) {
-        temp[item.name] = item.value;
-        if (item.name === 'cop') {
-            array.push(temp);
-            temp = {};
-        }
+  var res = {};
+  var temp = {};
+  var array = [];
+  for (var item of data) {
+    temp[item.name] = item.value;
+    if (item.name === "cop") {
+      array.push(temp);
+      temp = {};
     }
-    for (let index = array.length; index >= 0; index--) {
-        const element = array[index];
-        if (index > 0)
-            array[index - 1].child = element;
-    }
-    res = array.length > 0 ? array[0] : {};
-    return res;
+  }
+  for (let index = array.length; index >= 0; index--) {
+    const element = array[index];
+    if (index > 0) array[index - 1].child = element;
+  }
+  res = array.length > 0 ? array[0] : {};
+  return res;
 }
 
-function resetForm(){
-    $('#result-table').bootstrapTable('destroy');
-    $('#result-table').hide();
-    $('#query-builder tbody').empty();
-    addRow();
+function resetForm() {
+  $("#result-table").bootstrapTable("destroy");
+  $("#result-table").hide();
+  $("#query-builder tbody").empty();
+  addRow();
+}
+
+function fillOperatorsDropdown(ops, row) {
+  let opSelector = row.find("#operators");
+  opSelector.empty();
+  for (let i = 0; i < ops.length; i++) {
+    let operatorOption = $("<option></option>", {
+      value: ops[i].value
+    });
+    operatorOption.text(ops[i].title);
+    opSelector.append($(operatorOption));
+  }
+}
+
+function fillFieldsDropdown(fields, row) {
+  let fieldSelector = row.find("#all-fields");
+    for (let i = 0; i < fields.length; i++) {
+    let fieldOption = $("<option></option>", {
+      value: fields[i].field,
+      datatype: fields[i].type
+    });
+    fieldOption.text(fields[i].field.toUpperCase());
+    fieldSelector.append($(fieldOption));
+  }
 }
