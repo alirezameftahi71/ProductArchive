5f7c7827edb8a24e393b0b6b6b28620ef1abb206
Alireza Meftahi <alirezameftahi_71@yahoo.com>
2019-08-06 23:09:26 +0430

    added clockwork

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,9 @@ use App\Game;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::get('/games', 'GameController@all');
 Route::get('/games/{game}', 'GameController@show');
