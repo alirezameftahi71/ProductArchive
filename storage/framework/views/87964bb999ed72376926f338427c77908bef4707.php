b75ae2b102a01f9f34a369cd1937d930568f8fe7
Alireza Meftahi <alirezameftahi_71@yahoo.com>
2019-08-17 21:30:55 +0430

    removed files

--- storage/framework/views/87964bb999ed72376926f338427c77908bef4707.php
+++
@@ -1,3 +0,0 @@
-<footer id='footer' class="container-fluid text-center bg-dark text-light">
-	&copy; <?php echo e(2018 != date('Y') ? '2017 - ' . date('Y') : 2017); ?>  Alireza Meftahi | All Rights Reserved
-</footer><?php /**PATH /data/data/com.termux/files/home/dev/www/ProductArchive/resources/views/layouts/footer.blade.php ENDPATH**/ ?>
