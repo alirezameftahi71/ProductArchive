1ec48f7ae60283f06e7793fb9c708d57c98afbea
alirezameftahi71 <alirezameftahi_71@yahoo.com>
2020-05-24 23:14:22 +0430

    two way binding input tags

--- resources/js/components/tag-autocomplete.vue
+++ resources/js/components/tag-autocomplete.vue
@@ -1,55 +1,62 @@
 <template>
-  <div>
-    <b-form-tags :input-id="inputid" v-model="value" no-outer-focus class="mb-2 no-bg" :separator="seperators">
-      <template v-slot="{ tags, inputAttrs, inputHandlers, tagVariant, addTag, removeTag }">
-        <b-input-group class="mb-2">
-          <b-form-input
-            v-bind="inputAttrs"
-            ref="input"
-            v-on:input="onInputChange"
-            v-on="inputHandlers"
-            placeholder="To add tags press ; , or enter"
-            class="form-control"
-          ></b-form-input>
-          <b-input-group-append>
-            <b-button @click="addTag()" variant="outline-secondary">Add</b-button>
-          </b-input-group-append>
-        </b-input-group>
-        <div class="dropdown-menu dropdown-items-container" :class="{ show: isDropdownVisible }">
-          <a
-            class="dropdown-item"
-            v-for="item in suggestItems"
-            :key="item.id"
-            href="#"
-            @click="
-              addTag(item.name);
-              isDropdownVisible = false;
-            "
-            >{{ item.name }}</a
-          >
-        </div>
+  <b-form-tags
+    :input-id="inputid"
+    v-model="values"
+    @input="$emit('update:selectedtags', values)"
+    no-outer-focus
+    class="p-0 no-bg"
+    :separator="seperators"
+  >
+    <template v-slot="{ tags, inputAttrs, inputHandlers, tagVariant, addTag, removeTag }">
+      <b-input-group class="mb-2">
+        <b-form-input
+          v-bind="inputAttrs"
+          ref="input"
+          v-on:input="onInputChange"
+          v-on="inputHandlers"
+          placeholder="To add tags press ; , or enter"
+          class="form-control"
+        ></b-form-input>
+        <b-input-group-append>
+          <b-button @click="addTag()" variant="outline-secondary">Add</b-button>
+        </b-input-group-append>
+      </b-input-group>
+      <div class="dropdown-menu dropdown-items-container" :class="{ show: isDropdownVisible }">
+        <a
+          class="dropdown-item"
+          v-for="item in suggestItems"
+          :key="item.id"
+          href="#"
+          @click="
+            addTag(item.name);
+            isDropdownVisible = false;
+          "
+          >{{ item.name }}</a
+        >
+      </div>
 
-        <div class="d-inline-block">
-          <b-form-tag v-for="tag in tags" @remove="removeTag(tag)" :key="tag" :title="tag" :variant="tagVariant" class="mr-1">{{
-            tag
-          }}</b-form-tag>
-        </div>
-      </template>
-    </b-form-tags>
-  </div>
+      <div :class="{ 'd-inline-block': iscontainerstatic }">
+        <b-form-tag v-for="tag in tags" @remove="removeTag(tag)" :key="tag" :title="tag" :variant="tagVariant" class="mr-1">{{
+          tag
+        }}</b-form-tag>
+      </div>
+    </template>
+  </b-form-tags>
 </template>
 
 <script>
 export default {
   props: {
     inputid: String,
-    api: String
+    api: String,
+    iscontainerstatic: Boolean,
+    selectedtags: Array
   },
   data() {
     return {
+      values: this.selectedtags,
       isDropdownVisible: false,
       seperators: ",;",
-      value: [],
       suggestItems: []
     };
   },
